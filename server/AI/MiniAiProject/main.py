65c25b20c689a3332bb7a59726277085e5812611
amr9 <amr12201148@yahoo.com>
2023-02-26 00:03:38 +0200

    send and download video with 2 srt

--- server/AI/MiniAiProject/main.py
+++ server/AI/MiniAiProject/main.py
@@ -16,40 +16,6 @@ import mediapipe as mp
 
 
 
-def draw_styled_landmarks(image, results):
-    mp_drawing = mp.solutions.drawing_utils
-    mp_holistic = mp.solutions.holistic
-    # Draw face connections
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-                              mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
-                              mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
-                              )
-    # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
-                              )
-    # Draw left hand connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
-                              )
-    # Draw right hand connections
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-                              )
-
-
-def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
-    image.flags.writeable = False  # Image is no longer writeable
-    results = model.process(image)  # Make prediction
-    image.flags.writeable = True  # Image is now writeable
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGB 2 BGR
-    return image, results
-
-
 # creation of csv file for the landmarks cords
 def create_landmarks_cords():
     num_cords = 543
@@ -148,19 +114,6 @@ def extract_landmarks(results, action_name):
     return row
 
 
-def extract_keypoints(results):
-    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in
-                     results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33 * 4)
-    face = np.array([[res.x, res.y, res.z, res.visibility] for res in
-                     results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468 * 4)
-    lh = np.array([[res.x, res.y, res.z, res.visibility] for res in
-                   results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 4)
-    rh = np.array([[res.x, res.y, res.z, res.visibility] for res in
-                   results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
-        21 * 4)
-    return np.concatenate([pose, face, lh, rh])
-
-
 def train_model():
     df = pd.read_csv('coords.csv')
 
@@ -188,7 +141,7 @@ def train_model():
     with open('action_detection.pkl', 'wb') as f:
         pickle.dump(fit_models['rf'], f)
 
-
+# old model code
 def test_model():
     with open('body_language.pkl', 'rb') as f:
         model = pickle.load(f)
@@ -306,34 +259,28 @@ def meaning_action(action):
         if action == row[0]:
             return row[2]
 
+# new model code
+def test_model_new(path_with_file_extension,destination):
 
-def test_model_new():
-    filename = filedialog.askopenfilename(initialdir="/",
-                                          title="Select a File",
-                                          filetypes=[
-                                              ("video", ".mp4"),
-                                              ("video", ".MP4"),
-                                              ("video", ".avi"),
-                                              ("video", ".AVI"),
-                                          ])
+    res = path_with_file_extension.split('/')
+    x = res[-1]
+    x = x[::-1].split('.', 1)[1][::-1]
+    filename = x
 
     with open('action_detection.pkl', 'rb') as f:
         model = pickle.load(f)
 
-    mp_drawing = mp.solutions.drawing_utils
     mp_holistic = mp.solutions.holistic
 
-    # 1. decleration of variables
-    start=0
-    end=0
+    # 1. declaration of variables
     sequence = []
     sentence = []
     predictions = []
     counter = [0]
-    threshold = 0.3
+    threshold = 0.6
 
-    #read video frames
-    cap = cv2.VideoCapture(filename)
+    # read video frames
+    cap = cv2.VideoCapture(path_with_file_extension)
 
     # getting the width, height, frame_no, framerate(fps) and the frame number of the video for the video writer
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -342,9 +289,10 @@ def test_model_new():
     framcount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     frame_no = 0
 
-    #puting the video wirter into variable
-    writer= cv2.VideoWriter(
-        'C:/Users/amr12/Downloads/Video/video.mp4', cv2.VideoWriter_fourcc(*'DIVX'), fps, (width,height)
+    # puting the video wirter into variable
+    writer = cv2.VideoWriter(
+        '{path}/{filename}'.format(path=destination, filename=res[-1])
+        , cv2.VideoWriter_fourcc(*'DIVX'), fps, (width, height)
     )
 
     # Set mediapipe model
@@ -357,8 +305,8 @@ def test_model_new():
                 break
             # Make detections
             image, results = mediapipe_detection(curr_frame, holistic)
-            #frame with timestamp in seconds
-            print("for frame : " + str(frame_no) + "   timestamp is: ", str((cap.get(cv2.CAP_PROP_POS_MSEC)/1000)))
+            # frame with timestamp in seconds
+            print("for frame : " + str(frame_no) + "   timestamp is: ", str((cap.get(cv2.CAP_PROP_POS_MSEC) / 1000)))
 
             print(results)
 
@@ -378,14 +326,15 @@ def test_model_new():
 
                 # if the last 10 predictions are the same
                 if np.unique(predictions[-10:])[0] == res:
-                    #if the prediction is higher than threshold
+                    # if the prediction is higher than threshold
                     if body_language_prob[np.argmax(body_language_prob)] > threshold:
-                        #if it is not the first prediction
+                        # if it is not the first prediction
                         if len(sentence) > 0:
-                            #if the predictions is not equal to the last prediction
+                            # if the predictions is not equal to the last prediction
                             if res != sentence[-1]:
-                                #end time
-                                end = (cap.get(cv2.CAP_PROP_POS_MSEC)/1000)
+                                # end time
+                                end = (cap.get(cv2.CAP_PROP_POS_MSEC) / 1000)
+
                                 def f(x, decimals=3):
                                     r = str(round(x, decimals))  # round and convert to string
                                     r = r.split('.')[-1]  # split at the dot and keep the decimals
@@ -407,13 +356,13 @@ def test_model_new():
                                 hours_end = int(end / 3600)
                                 print(f"{hours_end:02}:{minutes_end:02}:{seconds_end:02},{int(f(milliseconds_end)):03}")
 
-                                with open("Actions.srt", "a") as srt_file:
+                                with open(f"{destination}/{filename}.srt", "a") as srt_file:
                                     srt_file.write(
                                         f"{counter[-1]}\n{hours_start:02}:{minutes_start:02}:{seconds_start:02},{int(f(milliseconds_start)):03}"
                                         f" --> {hours_end:02}:{minutes_end:02}:{seconds_end:02},{int(f(milliseconds_end)):03}\n{sentence[-1]}\n\n")
                                 meaning = meaning_action(sentence[-1])
                                 print(meaning)
-                                with open("Meaning.srt", "a") as srt_file:
+                                with open(f"{destination}/{filename}_meaning.srt", "a") as srt_file:
                                     srt_file.write(
                                         f"{counter[-1]}\n{hours_start:02}:{minutes_start:02}:{seconds_start:02},{int(f(milliseconds_start)):03}"
                                         f" --> {hours_end:02}:{minutes_end:02}:{seconds_end:02},{int(f(milliseconds_end)):03}\n{meaning}\n\n")
@@ -423,14 +372,14 @@ def test_model_new():
                                 sentence.append(res)
                                 print(sentence)
 
-                                #start time again
-                                start2 = (cap.get(cv2.CAP_PROP_POS_MSEC)/1000)
+                                # start time again
+                                start2 = (cap.get(cv2.CAP_PROP_POS_MSEC) / 1000)
 
-                        #else of the first prediction
+                        # else of the first prediction
                         else:
                             # start time
-                            start2 = (cap.get(cv2.CAP_PROP_POS_MSEC)/1000)
-                            #counter for the SRT file actions
+                            start2 = (cap.get(cv2.CAP_PROP_POS_MSEC) / 1000)
+                            # counter for the SRT file actions
                             counter.append(counter[-1] + 1)
                             sentence.append(res)
 
@@ -440,7 +389,7 @@ def test_model_new():
                 # write the video frame to the device
                 writer.write(image)
 
-                #Get status box
+                # Get status box
                 cv2.rectangle(image, (0, 0), (250, 60), (245, 117, 16), -1)
                 cv2.putText(image, 'PROB'
                             , (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
@@ -452,7 +401,7 @@ def test_model_new():
                             , (95, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
                 cv2.putText(image, res.split(' ')[0]
                             , (90, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-            #increment the frame by 1
+            # increment the frame by 1
             frame_no += 1
 
             # Show to screen
@@ -461,7 +410,8 @@ def test_model_new():
             # Break by pressing Q
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
-        #this allows for the last action/prediction to be saved even if there is only one action (492->524)
+
+        # this allows for the last action/prediction to be saved even if there is only one action (492->524)
         def f(x, decimals=3):
             r = str(round(x, decimals))  # round and convert to string
             r = r.split('.')[-1]  # split at the dot and keep the decimals
@@ -472,20 +422,20 @@ def test_model_new():
         minutes_start = int(start2 / 60) % 60
         hours_start = int(start2 / 3600)
 
-        end = framcount/fps
+        end = framcount / fps
         milliseconds_end = end % 1
         seconds_end = int(end) % 60
         minutes_end = int(end / 60) % 60
         hours_end = int(end / 3600)
         print(f"{hours_end:02}:{minutes_end:02}:{seconds_end:02},{int(f(milliseconds_end)):03}")
 
-        with open("Actions.srt", "a") as srt_file:
+        with open(f"{destination}/{filename}.srt", "a") as srt_file:
             srt_file.write(
                 f"{counter[-1]}\n{hours_start:02}:{minutes_start:02}:{seconds_start:02},{int(f(milliseconds_start)):03}"
                 f" --> {hours_end:02}:{minutes_end:02}:{seconds_end:02},{int(f(milliseconds_end)):03}\n{sentence[-1]}\n\n")
         meaning = meaning_action(sentence[-1])
         print(meaning)
-        with open("Meaning.srt", "a") as srt_file:
+        with open(f"{destination}/{filename}_meaning.srt", "a") as srt_file:
             srt_file.write(
                 f"{counter[-1]}\n{hours_start:02}:{minutes_start:02}:{seconds_start:02},{int(f(milliseconds_start)):03}"
                 f" --> {hours_end:02}:{minutes_end:02}:{seconds_end:02},{int(f(milliseconds_end)):03}\n{meaning}\n\n")
@@ -494,6 +444,53 @@ def test_model_new():
         counter.append(counter[-1] + 1)
         cap.release()
         cv2.destroyAllWindows()
+
+
+def draw_styled_landmarks(image, results):
+    mp_drawing = mp.solutions.drawing_utils
+    mp_holistic = mp.solutions.holistic
+    # Draw face connections
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                              mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                              mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                              )
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                              )
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                              )
+    # Draw right hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
+
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False  # Image is no longer writeable
+    results = model.process(image)  # Make prediction
+    image.flags.writeable = True  # Image is now writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                     results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33 * 4)
+    face = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                    results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468 * 4)
+    lh = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                   results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 4)
+    rh = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                   results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
+        21 * 4)
+    return np.concatenate([pose,face, lh, rh])
 
 
 window = Tk()
