96f6dc0a8843c3d7ec620b107dc098a1849da827
amr9 <amr12201148@yahoo.com>
2023-05-10 20:14:40 +0300

    1

--- server/AI/MiniAiProject/main.py
+++ server/AI/MiniAiProject/main.py
@@ -226,134 +226,23 @@ def train_model():
         pickle.dump(fit_models['lr'], f)
 
 
-# with open('pose_nb.pkl', 'wb') as f:
-#    pickle.dump(fit_models['nb'], f)
-#
-# with open('pose_knn.pkl', 'wb') as f:
-#     pickle.dump(fit_models['knn'], f)
-#
-# with open('pose_rc.pkl', 'wb') as f:
-#     pickle.dump(fit_models['rc'], f)
-#
-# with open('pose_rf.pkl', 'wb') as f:
-#     pickle.dump(fit_models['rf'], f)
-#
-# with open('pose_gb.pkl', 'wb') as f:
-#     pickle.dump(fit_models['gb'], f)
-#
-# with open('pose_dt.pkl', 'wb') as f:
-#     pickle.dump(fit_models['dt'], f)
-
-
-# old model
-def test_model():
-    with open('body_language.pkl', 'rb') as f:
-        model = pickle.load(f)
-
-    mp_drawing = mp.solutions.drawing_utils
-    mp_holistic = mp.solutions.holistic
-
-    cap = cv2.VideoCapture("D:/Coding/BodyLanguageDecoderV2/Studio Project — Kapwing.mp4")
-    # Initiate holistic model
-    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-
-        while cap.isOpened():
-            ret, frame = cap.read()
-
-            # Recolor Feed
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image.flags.writeable = False
-
-            # Make Detections
-            results = holistic.process(image)
-            # print(results.face_landmarks)
-
-            # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
-
-            # Recolor image back to BGR for rendering
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-            # 1. Draw face landmarks
-            mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-                                      mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
-                                      mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
-                                      )
-
-            # 2. Right hand
-            mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                                      mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
-                                      mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
-                                      )
-
-            # 3. Left Hand
-            mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                                      mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-                                      mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
-                                      )
-
-            # 4. Pose Detections
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
-                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-                                      )
-            # Export coordinates
-            try:
-                row = extract_keypoints(results)
-
-                #             # Append class name
-                #             row.insert(0, class_name)
-
-                #             # Export to CSV
-                #             with open('coords.csv', mode='a', newline='') as f:
-                #                 csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                #                 csv_writer.writerow(row)
-
-                # Make Detections
-                X = pd.DataFrame([row])
-                body_language_class = model.predict(X)[0]
-                body_language_prob = model.predict_proba(X)[0]
-                print(body_language_class, body_language_prob)
-
-                # Grab ear coords
-                coords = tuple(np.multiply(
-                    np.array(
-                        (results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].x,
-                         results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].y))
-                    , [640, 480]).astype(int))
-
-                cv2.rectangle(image,
-                              (coords[0], coords[1] + 5),
-                              (coords[0] + len(body_language_class) * 20, coords[1] - 30),
-                              (245, 117, 16), -1)
-                cv2.putText(image, body_language_class, coords,
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-                # Get status box
-                cv2.rectangle(image, (0, 0), (250, 60), (245, 117, 16), -1)
-
-                # Display Class
-                cv2.putText(image, 'CLASS'
-                            , (95, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-                cv2.putText(image, body_language_class.split(' ')[0]
-                            , (90, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-                # Display Probability
-                cv2.putText(image, 'PROB'
-                            , (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-                cv2.putText(image, str(round(body_language_prob[np.argmax(body_language_prob)], 2))
-                            , (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-            except:
-                pass
-
-            cv2.imshow('Raw Webcam Feed', image)
-
-            if cv2.waitKey(10) & 0xFF == ord('q'):
-                break
-
-    cap.release()
-    cv2.destroyAllWindows()
+    # with open('pose_nb.pkl', 'wb') as f:
+    #    pickle.dump(fit_models['nb'], f)
+    #
+    # with open('pose_knn.pkl', 'wb') as f:
+    #     pickle.dump(fit_models['knn'], f)
+    #
+    # with open('pose_rc.pkl', 'wb') as f:
+    #     pickle.dump(fit_models['rc'], f)
+    #
+    # with open('pose_rf.pkl', 'wb') as f:
+    #     pickle.dump(fit_models['rf'], f)
+    #
+    # with open('pose_gb.pkl', 'wb') as f:
+    #     pickle.dump(fit_models['gb'], f)
+    #
+    # with open('pose_dt.pkl', 'wb') as f:
+    #     pickle.dump(fit_models['dt'], f)
 
 
 # new model code
@@ -609,7 +498,7 @@ def loop():
                     return 0
 
     # assign directory
-    directory = 'Turtle\\'
+    directory = 'C:\\Users\\amr12\\OneDrive\\Desktop\\test'
     if not os.path.exists(directory):
         os.mkdir(directory)
     folders = Path(directory).glob('*')
@@ -667,109 +556,3 @@ button_exit.pack()
 
 window.mainloop()
 
-# to be determined
-'''
-#don't use the uploadanddownlad function
-def UploadAndDownloadVideoLandmarks(filename):
-    landmarks=[]
-    destination_path ="C:/Users/amr12/PycharmProjects/MiniAiProject/"
-    source_path ="C:/Users/amr12/OneDrive/Desktop/amr videos/"
-    # putting the solutions into small variables to call them
-    mp_drawing = mp.solutions.drawing_utils
-    mp_holistic = mp.solutions.holistic
-    # capturing the video applicable to change in the future
-    cap = cv2.VideoCapture(source_path+filename)
-    # getting the width and the height of the video for the viedo writer
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    # puting the video wirter into variable
-    writer = cv2.VideoWriter(
-        '{path}{filename}'.format(path=destination_path, filename=filename)
-        , cv2.VideoWriter_fourcc(*'mp4v'), 20, (width, height)
-    )
-    # Initiate holistic model
-    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-            # Recolor Feed
-            image = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
-            # make image not writeable for extra performance that doesn't exist ?
-            image.flags.writeable = False
-            # Make Detections
-            results = holistic.process(image)
-            #print(results.face_landmarks)
-            landmarks.append(results.face_landmarks.landmark)
-            # make image writeable again
-            image.flags.writeable = True
-            # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
-            # Recolor image back to BGR for rendering
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            # 1. Draw face landmarks
-            mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-                                      mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
-                                      mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
-                                      )
-            # 2. Right hand
-            mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                                      mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
-                                      mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
-                                      )
-            # 3. Left Hand
-            mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                                      mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-                                      mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
-                                      )
-            # 4. Pose Detections
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
-                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-                                      )
-            # write on disk the video with holistics
-            writer.write(image)
-            cv2.imshow('video', image)
-            if cv2.waitKey(10) & 0xFF == ord('q'):
-                break
-    cap.release()
-    writer.release()
-    cv2.destroyAllWindows()
-    print('length is ',len(landmarks))
-    return landmarks
-#UploadAndDownloadVideoLandmarks("arms 1.mp4")
-#print('land marks are ',UploadAndDownloadVideoLandmarks("arms 1.mp4"))
-'''
-
-'''
-#multi threading code for future improvement of the model
-from threading import Thread
-import cv2, time
-class ThreadedCamera(object):
-    def __init__(self, src=0):
-        self.capture = cv2.VideoCapture(src)
-        self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)
-        # FPS = 1/X
-        # X = desired FPS
-        self.FPS = 1/30
-        self.FPS_MS = int(self.FPS * 1000)
-        # Start frame retrieval thread
-        self.thread = Thread(target=self.update, args=())
-        self.thread.daemon = True
-        self.thread.start()
-    def update(self):
-        while True:
-            if self.capture.isOpened():
-                (self.status, self.frame) = self.capture.read()
-            time.sleep(self.FPS)
-    def show_frame(self):
-        cv2.imshow('frame', self.frame)
-        cv2.waitKey(self.FPS_MS)
-if __name__ == '__main__':
-    src = 'C:/Users/Asus/Documents/Studio Project — Kapwing.mp4'
-    threaded_camera = ThreadedCamera(src)
-    while True:
-        try:
-            threaded_camera.show_frame()
-        except AttributeError:
-            pass
-'''
